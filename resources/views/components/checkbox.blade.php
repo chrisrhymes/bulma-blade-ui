e97a32dd84bdb52cdf0d659077777c79e30ff7f7
chrisrhymes <webdesigncsr@gmail.com>
2021-02-18 21:33:08 +0000

    Add editor config file

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,13 +1,13 @@
 @props(['label', 'name' => '', 'value' => '', 'required' => false])
 <label class="checkbox" for="{{ \Illuminate\Support\Str::camel($name.$value) }}">
-  <input type="checkbox" name="{{ $name }}"
-     id="{{ \Illuminate\Support\Str::camel($name) }}"
-     @if($required) required @endif
-     @if($attributes->has('wire:model'))
-        wire:model="{{ $attributes->whereStartsWith('wire:model')->first() }}"
-     @endif
-     value="{{ $value }}"
-  >
-    {{ $label }}
+    <input type="checkbox" name="{{ $name }}"
+        id="{{ \Illuminate\Support\Str::camel($name) }}"
+        @if($required) required @endif
+        @if($attributes->has('wire:model'))
+            wire:model="{{ $attributes->whereStartsWith('wire:model')->first() }}"
+        @endif
+        value="{{ $value }}"
+    >
+        {{ $label }}
 </label>
 <x-bbui::error name="{{ $name }}"></x-bbui::error>
